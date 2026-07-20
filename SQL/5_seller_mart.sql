1c2aaf10d0027e77d49234a3344c7adc736f7e61
Anastasiia <eg.nastyaa@gmail.com>
2026-07-20 21:29:48 +0300

    переименован столбец

--- SQL/5_seller_mart.sql
+++ SQL/5_seller_mart.sql
@@ -1,3 +1,4 @@
+--DROP VIEW seller_mart;
 --CREATE VIEW seller_mart AS
 WITH seller_orders AS (
     SELECT
@@ -25,7 +26,7 @@ SELECT
     s.seller_city,
     s.seller_state,
     COUNT(so.order_id) AS total_orders,
-    COALESCE(SUM(so.sellers_revenue), 0) AS sellers_revenue,
+    COALESCE(SUM(so.sellers_revenue), 0) AS seller_revenue,
     COALESCE(ROUND(AVG(so.sellers_revenue), 2), 0) AS avg_sellers_revenue,
     COALESCE(SUM(so.seller_order_items_cnt), 0) AS total_items_sold,
     MIN(so.order_purchase_timestamp) AS first_order_date,
